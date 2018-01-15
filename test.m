c87758e6bdb78443df5a87833795d05b3318d348
unknown <>
2018-01-15 17:01:56 +0800

    add PIT

--- test.m
+++ test.m
@@ -1,10 +1,6 @@
-
-clc;clear;
-t = [0.001:0.001:4];
-sigA = sin(2*pi*20.2*t)+0.4*sin(2*pi*30.2*t+0.5);
-sigB = sin(2*pi*40.4*t)+0.4*sin(2*pi*60.4*t+0.5);
-sigC = sigA(2:2:end);
-
-sigA(1000:1010)
-sigB(1000:1010)
-sigC(1000:1010)
+clear;clc;
+x = [0:2/44100:2*pi];
+y = sin(x);
+xx = 0:.25:10;
+yy = spline(x,y,xx);
+plot(x,y,'o',xx,yy)
